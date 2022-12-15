7953d20b89f11376423913c1c4fc08f206a48d83
Idwes SOUGH <sough.idwes@gmail.com>
2022-12-15 09:56:28 +0100

    update script

--- src/NeuralNetwork/yolo/custom.py
+++ src/NeuralNetwork/yolo/custom.py
@@ -1,9 +1,19 @@
 import os
-import pandas as pd
-
+import pandas as pd 
+FILENAME_LENGHT = 8
 actual_path = os.getcwd() 
 file_list = os.listdir()
-listing = [os.path.join(actual_path, str(file)[0:8]+".jpg") for file in file_list] 
+for file in file_list:
+    if file.endswith(".jpg"):
+        filename = file[0:FILENAME_LENGHT] + ".jpg"
+    elif file.endswith(".txt"):
+        filename = file[0:FILENAME_LENGHT] + ".txt"
+    os.rename(file, filename)
+
+
+
+file_list = os.listdir()
+listing = [os.path.join(actual_path, str(file)) for file in file_list] 
 print(listing) 
 final_df = pd.DataFrame(listing) 
 print(final_df)#final_df.to_csv("files_which_ends_with_jpeg.csv", index=False)
