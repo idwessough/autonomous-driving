a3aacbc00246f4527adedb4be4baaa3106bce888
Idwes SOUGH <sough.idwes@gmail.com>
2022-12-15 09:32:52 +0100

    [ADD] ROSDarknet

--- src/NeuralNetwork/yolo/custom.py
+++ src/NeuralNetwork/yolo/custom.py
@@ -1,12 +1,9 @@
 import os
 import pandas as pd
 
-actual_path = os.getcwd()
+actual_path = os.getcwd() 
 file_list = os.listdir()
-listing = [actual_path+file for file in file_list]
-final_list=[]
-for file in listing: 
-    if file.endswith(".jpg"):
-        final_list.append(file)
-final_df = pd.DataFrame(final_list) 
-final_df.to_csv("files_which_ends_with_jpeg.csv", index=False)
+listing = [os.path.join(actual_path, str(file)[0:8]+".jpg") for file in file_list] 
+print(listing) 
+final_df = pd.DataFrame(listing) 
+print(final_df)#final_df.to_csv("files_which_ends_with_jpeg.csv", index=False)
