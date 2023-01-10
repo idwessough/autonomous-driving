1553c352267c6fe646e2ffba839f90574e08dad1
Idwes SOUGH <sough.idwes@gmail.com>
2023-01-10 17:33:31 +0100

    [ADD] Crop image to adapt view on road only and README.md

--- src/LineFollower/line_followerv.py
+++ src/LineFollower/line_followerv.py
@@ -10,23 +10,24 @@ threshold1 = 85
 threshold2 = 85
 theta=0
 
-minLineLength = 5
+minLineLength = 10
 maxLineGap = 10
 k_width = 5
 k_height = 5
-max_slider = 10
-
-# Linux System Serial Port
-# ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)           # linux
+max_slider = 10 
 
 # Read Image
 image = cv2.imread(os.path.join(os.path.dirname(__file__), 'test4.jpg'))
 image_size = image.shape
-r_width = int(image_size[1]/4)
 r_height = int(image_size[0]/4)
-# Resize width=500 height=500 incase of inputting raspi captured image
-print(r_width, r_height)
-image = cv2.resize(image,(r_width,r_height))
+r_width = int(image_size[1]/4)
+print(r_height, r_width)
+# Resize image 
+image = cv2.resize(image,(r_height, r_width))
+print(image.shape)
+# Crop Image dive height by 2
+image = image[int(r_width/2.75):r_width, :] 
+cv2.imshow("Original Image",image)
 # Convert the image to gray-scale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # given input image, kernel width =5 height = 5, Gaussian kernel standard deviation
@@ -40,16 +41,19 @@ for x in range(0, len(lines)):
     for x1,y1,x2,y2 in lines[x]:
         cv2.line(image,(x1,y1),(x2,y2),(255,0,0),3)
         theta=theta+math.atan2((y2-y1),(x2-x1)) 
-        print(theta)
-threshold=10
+print(theta) 
+
+threshold=5
+
 if(theta>threshold):
        print("Go left")
 
 if(theta<-threshold):
     print("Go right")
-if(abs(theta)<threshold):
 
+if(abs(theta)<threshold): 
     print("Go straight")
+
 theta=0
 cv2.imshow("Gray Image",gray)
 cv2.imshow("blurred",blurred)
