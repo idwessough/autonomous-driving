2ec8b6cdafdc299f37f707d5a9d445aa1fc666be
Arthur <a.saunier@e-totem.fr>
2023-01-13 10:06:04 +0100

    lime follower script and dependencies

---
+++ ROS1/ROS1_ws/src/line_follower/scripts/line_detector.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import traceback
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import Float32
+
+class LineDetector:
+    def __init__(self):
+        print("Initializing line detector node")
+        # read rate config
+        self.rate = rospy.Rate(rospy.get_param("/rate/lineDetector")) 
+        self.image_sub_comp = rospy.Subscriber("/camera/rgb/image_rect_color/compressed", CompressedImage, self.image_callback_compressed)
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_rect_color", Image, self.image_callback_raw)
+        self.image_pub = rospy.Publisher("processed_image", Image)
+        self.bridge = CvBridge()
+        self.line_offset = 0
+
+    def image_callback_raw(self, msg):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "passthrough")
+            self.process_image(cv_image)
+        except Exception as e:
+            traceback.print_exc()
+            rospy.logerr(e)
+            rospy.logerr("CvBridge Error, skipped image frame!")
+
+    def image_callback_compressed(self, msg):
+        try:
+            np_arr = np.fromstring(msg.data, np.uint8)
+            image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            self.process_image(image_np)
+        except Exception as e:
+            rospy.logerr(e)
+            rospy.logerr("skipped processed image frame!")
+
+    def process_image(self, cv_image):
+        # Downscale to 256x256
+        cv_image = cv2.resize(cv_image, (256, 256), interpolation = cv2.INTER_AREA)
+
+        # Change to grayscale and blur a bit
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        cv_image = cv2.GaussianBlur(cv_image, (7, 7), 0)
+
+        # Threshold the image
+        (T, threshold_image) = cv2.threshold(cv_image, 180, 255, cv2.THRESH_BINARY)
+
+        # Overlay black box on top of image
+        cv2.rectangle(threshold_image, (0, 0), (256, 128), (0,0,0), -1)
+
+        # Get center of the thresholded image
+        M = cv2.moments(threshold_image)
+
+        # calculate x,y coordinate of center
+        cX = 128
+        cY = 128
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+
+        # Compute an offset in [-1, 1] coordinates to convert to steering
+        self.line_offset = (cX - 128.0) / 128.0
+
+        # Change from grayscale
+        threshold_image = cv2.cvtColor(threshold_image, cv2.COLOR_GRAY2RGB)
+
+        # Overlay black box on top of image
+        cv2.rectangle(threshold_image, (0, 200), (256, 200), (255, 255, 255), -1)
+        
+        #Center of bottom section
+        threshold_image = cv2.circle(threshold_image, (cX, cY), 5, (0, 255, 0), -1)
+
+        # The centerline of the image
+        threshold_image = cv2.line(threshold_image,(128,0),(128,256),(0,0,255),1)
+
+        # Bottom quadrant that is used to signal which direction to go
+        threshold_image = cv2.line(threshold_image,(0,200),(256,200),(0,0,255),1)
+
+        # visualize line offset
+        threshold_image = cv2.line(threshold_image,(int(128 + (self.line_offset * 128)),0),(int(128 + (self.line_offset * 128)),256),(255,0,255),1)
+
+        # Output the processed message
+        image_message = self.bridge.cv2_to_imgmsg(threshold_image, "passthrough")
+        self.image_pub.publish(image_message)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+
+if __name__ =='__main__':
+    rospy.init_node('line_detector')
