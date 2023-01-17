51c9ca0f7334287b9a7c12a9da0756a75e9512e9
Arthur <a.saunier@e-totem.fr>
2023-01-17 21:35:25 +0100

    basic tf for stop implemented (not tested)

--- ROS1/ROS1_ws/src/nav_limo_projet/scripts/state_machine.py
+++ ROS1/ROS1_ws/src/nav_limo_projet/scripts/state_machine.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 
 import rospy
+import tf2_ros
+import tf
 import sys
+import traceback
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
+#from nav_msgs.msg import Odometry
 from std_msgs.msg import String
+
 
 class LimoEtat:
     def __init__(self):
         print("Initializing limo_etat node")
-        # config
-        self.rate = rospy.Rate(rospy.get_param("/rate/lineDetector")) 
-        self.rospy.Subscriber('/limo_angle', Float32 , self.callback_angle)
+        # config rate 
+        self.rate = rospy.Rate(rospy.get_param("/rate/nav_limo_projet")) 
+        # init subscriber/publisher
+        self.rospy.Subscriber('/limo_twist', Twist , self.callback_twist)
         self.rospy.Subscriber('/limo_action', String, self.callback_action)
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        # Tf buffer 
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
         #var pilotage
         self.x = 0
         self.z = 0
         self.Twist = Twist()
-        self.Action
-
+        # Var state machine
+        self.Action = None
+        self.flag_Action = None
 
     def action_vitesseNormale(self):
         self.x = 0.15
@@ -33,46 +43,59 @@ class LimoEtat:
     def action_virageDroite(self):
         self.z = 0.5
         self.action_vitesseNormale()
-
     
 
-       
-    def callback_angle(self, data):
-       
-       # Récupération de l'angle
-        self.z = data.data
-
-        # Mise à jour de l'angle
-        self.Twist.angular.z = self.z
-
-        # Publish
-        self.cmd_vel_pub.publish(self.Twist)
-
+    def callback_twist(self, data):
+        try:
+            self.z = data.data.angular.z
+            self.Twist.angular.z = self.z
+            self.cmd_vel_pub.publish(self.Twist)
+        except Exception as e:
+            traceback.print_exc()
+            rospy.logerr(e)
+            rospy.logerr("twist_callback Error, skipped frame!")
     
     def callback_action(self, data):
-
         # Récupération de l'action
         self.Action = data.data
-        
         if self.Action == "STOP":
-            self.action_stop()
-
+            self.flag_Action = 1 #Stop
         elif self.Action == "RALENTIR":
-            self.action_ralentir()
-
+            self.flag_Action = 2 #Ralentir
         elif self.Action == "VIRAGEDROITE":
-            self.action_virageDroite()
+            self.flag_Action = 3 #VirageDroite
+        elif self.Action == "PASSAGEPIETON":
+            self.flag_Action = 4 #PassagePieton
+        elif self.Action == "PIETON":
+            self.flag_Action = 5 #Pieton
 
+    # State Machine
+    def state_machine(self, Action_flag):
+        if self.flag_Action == 1: #Stop
+            try:
+                trans_stop = self.tfBuffer.lookup_transform('Stop', 'base_link', rospy.Time(0))
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                pass
+
+            msg = Twist()
+
+            msg.angular.z = 4 * math.atan2(trans.transform.translation.y, trans.transform.translation.x)
+            msg.linear.x = 0.5 * math.sqrt(trans.transform.translation.x ** 2 + trans.transform.translation.y ** 2)
+
+        elif self.flag_Action == 2: #Ralentir
+            
+        elif self.flag_Action == 3: #VirageDroite
+
+        elif self.flag_Action == 4: #PassagePieton
+
+        elif self.flag_Action == 5: #Pieton
+            
         else:
             self.action_vitesseNormale()
-            
-       
-        # Mise à jour de x
-        self.Twist.linear.x = self.x
 
-        # Publish
-        self.cmd_vel_pub.publish(self.Twist)
-
+    def run(self):
+        while not rospy.is_shutdown():
+            self.rate.sleep()
 
 def main(args):
     rospy.init_node('limo_state_machine')
