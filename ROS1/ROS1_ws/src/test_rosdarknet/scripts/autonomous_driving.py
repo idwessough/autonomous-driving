c1c8fe80541931d6678742f43017afe4408788c7
Arthur <a.saunier@e-totem.fr>
2023-01-18 22:53:25 +0100

    transform to class type script

--- ROS1/ROS1_ws/src/test_rosdarknet/scripts/autonomous_driving.py
+++ ROS1/ROS1_ws/src/test_rosdarknet/scripts/autonomous_driving.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2
 
 import rospy
+import sys
 # tf imports
 import tf2_ros
 import tf2_geometry_msgs
@@ -16,132 +17,148 @@ from darknet_ros_msgs.msg import BoundingBoxes
 import message_filters 
 import numpy as np
 
-tfBuffer = tf2_ros.Buffer()
+camera_angle = 67.9
 
-def read_cameras():
-    """_summary_: This function is used to read the images from the cameras (Darknet Depth) and to synchronize them.
-    """    
-    # Subscribe to ROSDARKNET to have the bounding boxes
-    DarknetData = message_filters.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes)
-    # Subscribe to depth CAMERA to have the depth image
-    DepthImage = message_filters.Subscriber("/camera/depth/image_raw", Image)
-    print("STARTING DATA SYNCHRONIZATION BETWEEN CAMERA AND DARKNET")
-    # Synchronize images 
-    ts = message_filters.ApproximateTimeSynchronizer([DarknetData, DepthImage], queue_size=10, slop=2.5)
-    # Using time sync to call the common callback function
-    ts.registerCallback(images_callback)
-    rospy.spin()
+class sign_identification:
+    def __init__(self):
+        print("Initializing limo_etat node")
+        # config rate 
+        self.rate = rospy.Rate(rospy.get_param("/rate/sign_identification")) 
+        # init subscriber/publisher
+        # Subscribe to ROSDARKNET to have the bounding boxes
+        self.DarknetData = message_filters.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes)
+        # Subscribe to depth CAMERA to have the depth image
+        self.DepthImage = message_filters.Subscriber("/camera/depth/image_raw", Image)
+        #publisher for actions to follow for limo
+        self.limo_publisher = rospy.Publisher('/limo_action', String, queue_size=10)
+        # Tf buffer and listener
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        # Synchronize images 
+        ts = message_filters.ApproximateTimeSynchronizer([self.DarknetData, self.DepthImage], queue_size=10, slop=2.5)
+        # Using time sync to call the common callback function
+        ts.registerCallback(self.images_callback)
 
-def images_callback(DarknetData, DepthImage):
-    listener = tf2_ros.TransformListener(tfBuffer)
-    person_detected = False
-    bridge = CvBridge() 
-    print("{nb} Objects detected".format(nb=len(DarknetData.bounding_boxes)))
-    depth_image = bridge.imgmsg_to_cv2(DepthImage)
-    print(depth_image.shape)
-    nearest_distance = 100000
-    nearest_object = None
-    # Loop through bounding boxes
-    for box in DarknetData.bounding_boxes: #box.xmin, box.xmax, box.ymin, box.ymax 
-        
-        #rospy.loginfo(box) 
-        # Compute center of bounding box
-        Xbounding_center = (box.xmin + box.xmax)/2
-        Ybounding_center = (box.ymin + box.ymax)/2
-        #object_distance = depth_image[box.ymin:box.ymax, box.xmin:box.xmax].mean()
-        object_distance = depth_image[Ybounding_center, Xbounding_center]
-        # Avoid 0 values (no depth made by an hadware offset)
-        if object_distance != 0: 
-            # Check if person is detected
-            if box.Class == "Person":
-                person_detected = True
-                person_distance = object_distance
-            # Check if object is closer than the previous one
-            elif nearest_distance > object_distance:
-                nearest_distance = object_distance
-                nearest_object = box
-            else:
-                continue
-    # Special case if a person is detected near from the car
-    if person_detected:
-        if person_distance <= 420:
-            action_todo = "PERSON"
-    # If there is another nearest object
-    if nearest_object is not None: 
-        print("Nearest object : {nearest_object.Class} at {nearest_distance} mm".format(nearest_object=nearest_object, nearest_distance=nearest_distance))
-        reaction_distance = 750    
-        object_class = nearest_object.Class
-        # Check if distance is close enough to react
-        if nearest_distance <= reaction_distance:
-            if object_class == "Stop": 
-                action_todo = "STOP" 
+    def images_callback(self, DarknetData, DepthImage):
+        person_detected = False
+        bridge = CvBridge() 
+        print("{nb} Objects detected".format(nb=len(DarknetData.bounding_boxes)))
+        depth_image = bridge.imgmsg_to_cv2(DepthImage)
+        print(depth_image.shape)
+        nearest_distance = 100000
+        nearest_object = None
+        # Loop through bounding boxes
+        for box in DarknetData.bounding_boxes: #box.xmin, box.xmax, box.ymin, box.ymax 
             
-            elif object_class == "CederPassage":
-                action_todo = "SLOW" 
-
-            # If there is a person near from the traffic sign
-            elif object_class == "PassagePieton": 
-                if person_detected and person_distance-100 <= nearest_distance <= person_distance+100:
-                    action_todo = "PASSAGEPIETON"
+            #rospy.loginfo(box) 
+            # Compute center of bounding box
+            Xbounding_center = (box.xmin + box.xmax)/2
+            Ybounding_center = (box.ymin + box.ymax)/2
+            #object_distance = depth_image[box.ymin:box.ymax, box.xmin:box.xmax].mean()
+            object_distance = depth_image[Ybounding_center, Xbounding_center]
+            # Avoid 0 values (no depth made by an hadware offset)
+            if object_distance != 0: 
+                # Check if person is detected
+                if box.Class == "Person":
+                    person_detected = True
+                    person_distance = object_distance
+                # Check if object is closer than the previous one
+                elif nearest_distance > object_distance:
+                    nearest_distance = object_distance
+                    nearest_object = box
                 else:
-                    pass 
-            
-            elif object_class == "rouge": 
-                    action_todo = "REDLIGHT"
-            
-            elif object_class == "vert":
-                action_todo = "GREENLIGHT"
+                    continue
+        # Special case if a person is detected near from the car
+        if person_detected:
+            if person_distance <= 420:
+                action_todo = "PERSON"
+        # If there is another nearest object
+        if nearest_object is not None: 
+            print("Nearest object : {nearest_object.Class} at {nearest_distance} mm".format(nearest_object=nearest_object, nearest_distance=nearest_distance))
+            reaction_distance = 750    
+            object_class = nearest_object.Class
+            # Check if distance is close enough to react
+            if nearest_distance <= reaction_distance:
+                if object_class == "Stop": 
+                    action_todo = "STOP" 
                 
-            elif object_class == "VirageDroite":
-                action_todo = "RIGHT"
-        
-            elif object_class == "RoutePrioritaire":
-                action_todo = "PRIORITY"
+                elif object_class == "CederPassage":
+                    action_todo = "SLOW" 
+
+                # If there is a person near from the traffic sign
+                elif object_class == "PassagePieton": 
+                    if person_detected and person_distance-100 <= nearest_distance <= person_distance+100:
+                        action_todo = "PASSAGEPIETON"
+                    else:
+                        pass 
+                
+                elif object_class == "rouge": 
+                        action_todo = "REDLIGHT"
+                
+                elif object_class == "vert":
+                    action_todo = "GREENLIGHT"
+                    
+                elif object_class == "VirageDroite":
+                    action_todo = "RIGHT"
             
-            # TODO : Add other cases after that    
+                elif object_class == "RoutePrioritaire":
+                    action_todo = "PRIORITY"
+                
+                # TODO : Add other cases after that    
+                else:
+                    print("soon change")
+                    action_todo = "OBJECT PAS ENCORE PRIS EN CHARGE" 
+                
+                # compute coordinates of point
+                X, Y = self.compute_distance(nearest_distance)  
+
+                # Create new sign tf in odom
+                self.Create_tf(X, Y, action_todo)
+
             else:
-                print("soon charge")
-                action_todo = "OBJECT PAS ENCORE PRIS EN CHARGE" 
-                
-            # Compute X and Y coordinates of the center of the bounding box thanks to trigonometry
-            camera_angle = 67.9
-            camera_height = 1.5
-            camera_width = 1.6
-            camera_focal = 1
-            # Compute the angle of the object
-            object_angle = (camera_angle/2)# - (camera_angle * (nearest_object.xmin + nearest_object.xmax)/(2*camera_width))
-            X = abs(nearest_distance * np.sin(object_angle))
-            Y = abs(nearest_distance * np.cos(object_angle))
-            print("X = {X} mm, Y = {Y} mm".format(X=X, Y=Y))
+                action_todo = "GO"
+                print("continue")
 
-            #Tf at location of sign
-            msg = PoseStamped()
-            msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = "camera_depth_frame"
-            msg.pose.position.x = X
-            msg.pose.position.y = Y
-            msg.pose.position.z = 0
-            msg.pose.orientation = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
-            
-            transform = tfBuffer.lookup_transform('odom', msg.header.frame_id, rospy.Time(0), rospy.Duration(1.0))
-            pose_transformed = tf2_geometry_msgs.do_transform_pose(msg, transform)
+            # publish action_todo in ros topic
+            self.limo_publisher.publish(action_todo)
 
-            #Create new sign tf in respect to odom
-            sign_tf2_broadcaster.handle_sign_pose(pose_transformed, action_todo, "odom")
+    def Create_tf(self, X, Y, action_todo):
+        # poseStamped in camera_depth_frame
+        msg = PoseStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "camera_depth_frame"
+        msg.pose.position.x = X
+        msg.pose.position.y = Y
+        msg.pose.position.z = 0
+        msg.pose.orientation = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
+        
+        # transpose msg from /camera_depth to /odom frame
+        transform = self.tfBuffer.lookup_transform('odom', msg.header.frame_id, rospy.Time(0), rospy.Duration(1.0))
+        #pb quaternion vs euler a check
+        pose_transformed = tf2_geometry_msgs.do_transform_pose(msg, transform)
 
-        else:
-            action_todo = "GO"
-            print("continue")
-        # publish action_todo in ros topic
-        limo_publisher = rospy.Publisher('/limo_action', String, queue_size=10)
-        limo_publisher.publish(action_todo)
+        # Create new sign tf in respect to odom
+        sign_tf2_broadcaster.handle_sign_pose(pose_transformed, "odom", action_todo)
 
-        # Process images...
-        print(action_todo)
+    def compute_distance(self, nearest_distance):
+        # Compute X and Y coordinates in frame camera_depth for the center of the bounding box thanks to trigonometry
+        object_angle = (camera_angle/2)# - (camera_angle * (nearest_object.xmin + nearest_object.xmax)/(2*camera_width))
+        X = abs(nearest_distance * np.sin(object_angle))
+        Y = abs(nearest_distance * np.cos(object_angle))
+        print("X = {X} mm, Y = {Y} mm".format(X=X, Y=Y))
+        return X, Y
 
-if __name__ == '__main__':
-    rospy.init_node('my_node')
+    def run(self):
+            while not rospy.is_shutdown():
+                self.rate.sleep()
+
+def main(args):
+    rospy.init_node('sign_identification')
+    Sign_id = sign_identification()
     try:
-        read_cameras()
-    except rospy.ROSInterruptException:
-        pass
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        
+if __name__ =='__main__':
+    main(sys.argv)
--- ROS1/ROS1_ws/src/test_rosdarknet/scripts/sign_tf2_broadcaster.py
+++ ROS1/ROS1_ws/src/test_rosdarknet/scripts/sign_tf2_broadcaster.py
@@ -9,13 +9,13 @@ import geometry_msgs.msg
 from nav_msgs.msg import Odometry
 
 
-def handle_sign_pose(data, Sign_name, header_frame):
+def handle_sign_pose(data, header_frame, sign_name,):
     br = tf2_ros.StaticTransformBroadcaster()
     t = geometry_msgs.msg.TransformStamped()
 
     t.header.stamp = rospy.Time.now()
     t.header.frame_id = header_frame
-    t.child_frame_id = Sign_name
+    t.child_frame_id = sign_name
     t.transform.translation.x = data.pose.position.x
     t.transform.translation.y = data.pose.position.y
     t.transform.translation.z = 0.0
